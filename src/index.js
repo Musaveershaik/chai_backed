04a1d9cc3e2606a167c8934208e96aa1684d22ba
musa <abdulmusawwirshaik@gmail.com>
2024-03-01 06:39:48 +0530

    util code ApiError, ApiResponse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { error } from "console";
 
 dotenv.config({
     path: './.env'
 });
+
+connectDB()
+.then(() => {
+    app.on('error',(error) => {
+        console.log("ERROR",error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`⚙️ Server is running on port ${process.env.PORT || 3000}`);
+    });
+})
+.catch((error) => {
+    console.log(`Mongo DB connection error: ${error}`);
+})
